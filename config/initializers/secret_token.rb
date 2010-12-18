31ef6d5083ed1b9db43cc60283760152966a8707
Ian Ha <ianha0@gmail.com>
2010-12-18 15:16:20 -0500

    Initial import from straight 'rails new' command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabBlog::Application.config.secret_token = 'efbba4737d53fa8d8a4d82f47eeb7243d0de5e81dac2211895b1cd2a44eb5e82fe36dfdd64b2b39ae9116da8ae78b5e2f9eb5f1ac34c42d91ed81e0b21d56174'
