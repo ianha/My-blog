22164bcd9a5eb657fe3346c431af9a836197f6cd
Ian Ha <ianha0@gmail.com>
2010-12-19 13:48:23 -0500

    Added some new controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  # display all posts
+  def index
+    @posts = Post.all
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @posts }
+    end
+  end
+  
+  # Called when a user requests to 
+  def new
+    @post = Post.new 
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml { render :xml => @post }
+    end
+  end
+  
+  def create
+    @post = Post.new(params[:post])
+    
+    respond_to do |format|  
+      if @post.save
+        format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @post }
+    end
+  end
+end
